a6112d60b014e945f7fbfd5313ea6704e6a840a1
AlberdaNazarius <103203230+AlberdaNazarius@users.noreply.github.com>
2023-09-30 09:42:18 +0300

    Improved deleteExpiredOrders method

--- src/main/java/com/test_task/app/configuration/scheduler/OrderScheduler.java
+++ src/main/java/com/test_task/app/configuration/scheduler/OrderScheduler.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.stream.Collectors;
 
@@ -18,13 +19,19 @@ public class OrderScheduler {
   private static final int SCHEDULER_FIXED_RATE = 60000;
   private static final int EXPIRATION_TIME = 10;
 
+  /**
+   * Deletes expired orders after some given time.
+   * <p>
+   *   {@code EXPIRATION_TIME} here is configured in minutes and {@code SCHEDULER_FIXED_RATE} in milliseconds
+   * </p>
+   */
   @Scheduled(fixedRate = SCHEDULER_FIXED_RATE)
   @Transactional
   public void deleteExpiredOrders() {
-    final var now = ZonedDateTime.now().getMinute();
+    final var now = ZonedDateTime.now();
     final var notPaidOrders = orderRepository.findByIsPaid(false);
     final var expiredOrders = notPaidOrders.stream()
-            .filter(x -> now - x.getCreationDate().getMinute() >= EXPIRATION_TIME)
+            .filter(order -> Duration.between(order.getCreationDate(), now).toMinutes() >= EXPIRATION_TIME)
             .collect(Collectors.toList());
 
     if (!expiredOrders.isEmpty()) {
